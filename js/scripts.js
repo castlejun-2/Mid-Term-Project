379e26087a8503895518286f4f780e431348bf21
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-05-05 16:19:08 +0900

    6th commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
 document.getElementById('project').onclick = function () {
-  document.getElementsByClassName('container px-4 px-lg-5 main')[0].style.visibility = 'hidden';
+  document.getElementsByClassName('container px-4 px-lg-5 main')[0].style.display = 'none';
+  document.getElementsByClassName('container px-4 px-lg-5 project')[0].style.display = 'block';
   document.getElementById('home').classList.remove('active');
   document.getElementById('project').classList.add('active');
 };
 
 document.getElementById('home').onclick = function () {
-  document.getElementsByClassName('container px-4 px-lg-5 main')[0].style.visibility = 'visible';
+  document.getElementsByClassName('container px-4 px-lg-5 project')[0].style.display = 'none';
+  document.getElementsByClassName('container px-4 px-lg-5 main')[0].style.display = 'block';
   document.getElementById('project').classList.remove('active');
   document.getElementById('home').classList.add('active');
 };
